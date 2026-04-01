4224de782a281afaaf2dba323c386d9ce01aa41c
Sadeel-KH <s12323642@stu.najah.edu>
2026-04-02 02:29:18 +0300

    ReservationServiceTest 2/2 green

--- src/main/java/com/appointment/observer/SMSNotificationObserver.java
+++ src/main/java/com/appointment/observer/SMSNotificationObserver.java
@@ -1,5 +1,21 @@
 package com.appointment.observer;
 
-public class SMSNotificationObserver {
+import com.appointment.domain.User;
+import java.util.ArrayList;
+import java.util.List;
 
+public class SMSNotificationObserver implements Observer {
+
+    private List<String> sentMessages = new ArrayList<>();
+
+    @Override
+    public void update(User user, String message) {
+        String sms = "SMS to " + user.getPhone() + ": " + message;
+        sentMessages.add(sms);
+        System.out.println(sms);
+    }
+
+    public List<String> getSentMessages() {
+        return sentMessages;
+    }
 }
