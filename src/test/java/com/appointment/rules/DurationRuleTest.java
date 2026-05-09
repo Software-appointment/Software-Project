eae78d46ce9197374f9be084e5126ef001d709f3
جنى رياض رشدي خرمة <jojokharma2018@gmail.com>
2026-05-09 15:31:01 +0300

    Change DurationRuleTest methods to package-private

--- src/test/java/com/appointment/rules/DurationRuleTest.java
+++ src/test/java/com/appointment/rules/DurationRuleTest.java
@@ -16,51 +16,49 @@ import com.appointment.domain.Reservation;
  * @author Student A
  * @version 1.0
  */
-
-
-public class DurationRuleTest {
+class DurationRuleTest {
 
     private DurationRule rule;
     private MeetingRoom room;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         rule = new DurationRule();
         room = new MeetingRoom("R1", "Small Room", 10, "Small", Collections.emptyList());
     }
 
     @Test
-    public void testDurationValid_30Minutes() {
+    void testDurationValid_30Minutes() {
         Reservation reservation = new TestReservation("R001", "2026-03-20", "10:00", 30, room);
         assertTrue(rule.isValid(reservation), "30 minutes should be valid");
     }
 
     @Test
-    public void testDurationValid_60Minutes() {
+    void testDurationValid_60Minutes() {
         Reservation reservation = new TestReservation("R002", "2026-03-20", "10:00", 60, room);
         assertTrue(rule.isValid(reservation), "60 minutes should be valid");
     }
 
     @Test
-    public void testDurationValid_120Minutes() {
+    void testDurationValid_120Minutes() {
         Reservation reservation = new TestReservation("R003", "2026-03-20", "10:00", 120, room);
         assertTrue(rule.isValid(reservation), "120 minutes should be valid");
     }
 
     @Test
-    public void testDurationInvalid_45Minutes() {
+    void testDurationInvalid_45Minutes() {
         Reservation reservation = new TestReservation("R004", "2026-03-20", "10:00", 45, room);
         assertFalse(rule.isValid(reservation), "45 minutes should be invalid");
     }
 
     @Test
-    public void testDurationInvalid_90Minutes() {
+    void testDurationInvalid_90Minutes() {
         Reservation reservation = new TestReservation("R005", "2026-03-20", "10:00", 90, room);
         assertFalse(rule.isValid(reservation), "90 minutes should be invalid");
     }
 
     @Test
-    public void testDurationInvalid_0Minutes() {
+    void testDurationInvalid_0Minutes() {
         Reservation reservation = new TestReservation("R006", "2026-03-20", "10:00", 0, room);
         assertFalse(rule.isValid(reservation), "0 minutes should be invalid");
     }
@@ -70,7 +68,7 @@ public class DurationRuleTest {
      */
     private static class TestReservation extends Reservation {
 
-        public TestReservation(String id, String date, String time, int duration, MeetingRoom room) {
+        TestReservation(String id, String date, String time, int duration, MeetingRoom room) {
             super(id, date, time, duration, room);
         }
 
